045ac14b5bb4077bb615d6a3e70a4878467d7ea0
Sheriff M Hardy <shadydiallo99@gmail.com>
2023-02-17 16:35:12 +0100

    top component (search) complete
    The search component of the weather App

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '841ca66f41msh167a5e61d5020dfp1a8576jsndff56a66af15',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
